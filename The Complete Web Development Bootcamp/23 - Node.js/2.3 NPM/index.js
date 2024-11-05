61d4e0ec7be987783ae32281386350b0c8cd913d
adam-shen <113874468+adam-shen@users.noreply.github.com>
2024-11-05 19:14:16 +0000

    Learning about NPM's

---
+++ The Complete Web Development Bootcamp/23 - Node.js/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+var generateName = require('sillyname');
+
+
+var sillyname = generateName();
+
+
+console.log("My name is " + sillyname);
+
+// console.log(`My name is ${sillyName}.`)
+// String interpretation with backticks
